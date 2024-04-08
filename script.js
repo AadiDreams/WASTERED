c4ef8870cccb12d3a6e01b2ae75478a591066eb3
Aadithya V Anand <aadithyadreams@gmail.com>
2024-04-08 14:30:16 +0530

    backend

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+
+import { sql } from "@vercel/postgres";
+
+export default async function Cart({ params }) {
+  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+
+  return (
+    <div>
+      {rows.map((row) => (
+        <div key={row.id}>
+          {row.id} - {row.quantity}
+        </div>
+      ))}
+    </div>
+  );
+}
+
 function value(){
     const user=document.getElementById("username").value;
     const pass=document.getElementById("password").value;
